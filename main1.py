16caf24c58787495da854bedfcb0304e410d2c10
filip98122 <filip98122@gmail.com>
2023-11-27 18:55:56 +0100

    Pong recreation

--- main1.py
+++ main1.py
@@ -62,7 +62,7 @@ class Player_1:
         self.mode = mode
     def draw(self,window):
         pygame.draw.rect(window,pygame.Color(255,255,255),pygame.Rect(self.x, self.y, self.width,self.height))
-    def move(self,keys):
+    def move(self,keys,ball_y):
         if self.mode == 0:
             if self.y >= 0:
                 if keys[pygame.K_w]:
@@ -81,9 +81,19 @@ class Player_1:
             if self.y <= 675:
                 if keys[pygame.K_DOWN]:
                     self.y += self.speed
-
+        if self.mode == 2:
+            if self.y <= 675:
+                if ball_y > self.y + self.height / 2:
+                    self.y += self.speed
+                
+            if self.y >= 0:
+                if ball_y < self.y + self.height / 2:
+                    self.y -= self.speed
+            
+            
 p1 = Player_1(75,337.5,25,125,0.2,0)
 
+bot = Player_1(725,337.5,25,125,0.2,2)
 
 p2 = Player_1(725,337.5,25,125,0.2,1)
 
@@ -101,7 +111,7 @@ class Button:
         pygame.Rect(self.x, self.y, self.width,self.height)) # Draws a rectangle
         
         text_surface1 = myfont1.render(self.text, True, (255,0,0))
-        window.blit(text_surface1, (button.x + 20,button.y + button.height / 2 - 50))
+        window.blit(text_surface1, (self.x + 20,self.y + self.height / 2 - 50))
 
 class Ball:
     def __init__(self,x,y,speed,size,dx,dy):
@@ -147,29 +157,37 @@ class Ball:
             self.dy = -0.5
             
         if times <= -1:
-            self.x += self.speed * self.dx
-            self.y += self.speed * self.dy
+            self.x += (self.speed * self.dx)
+            self.y += (self.speed * self.dy)
         return [times,p1_score,p2_score]
 
 
+        
 
-l_players = []
+
 
 ball = Ball(400,400,0.3,15,-1,-0.5)
 
 
-button = Button(250,300,250,150, "Play")
+
+button = Button(200,300,250,150, "Play")
+
+play_bot = Button(200,550,350,150,"Play bot")
+
 predx = 0
 predy = 0
 times = 0
 main_menu = 1
+play_againts_bot = 0
 game = 0
 was_holding = False
 p1_score = 0
 p2_score = 0
+re = 0
+er = 0
 
 while True:
-    if game == 1:
+    if main_menu != 1:
         window.fill("Black")
     if main_menu == 1:
         window.fill("White")
@@ -186,19 +204,59 @@ while True:
         text_surface1 = myfont.render("Main menu", True, (255,0,0))
         window.blit(text_surface1, (button.x + button.width / 2 - 135,button.y + button.height / 2 - 325))
         button.draw(window,p1,myfont2)
+        play_bot.draw(window,p1,myfont2)
+        
         if button_colision(button.width,button.height,button.x,button.y,mousePos,mouseState):
             game = 1
             main_menu = 0
+            play_againts_bot = 0
+            
+        if button_colision(play_bot.width,play_bot.height,play_bot.x,play_bot.y,mousePos,mouseState):
+            game = 0
+            main_menu = 0
+            play_againts_bot = 1
+            
+    if play_againts_bot == 1:
+        text_surface11 = myfont.render(f"{int(p1_score)}", True, (255,255,255))
+        window.blit(text_surface11, (150,100))
+        text_surface111 = myfont.render(f"{int(p2_score)}", True, (255,255,255))
+        window.blit(text_surface111, (550,100))
         
-
+        ball_rect = pygame.Rect(ball.x - ball.size, ball.y - ball.size, ball.size * 2,ball.size * 2)
+        
+        p1_rect = pygame.Rect(p1.x, p1.y, p1.width,p1.height)
+        
+        p2_rect = pygame.Rect(p2.x, p2.y, p2.width,p2.height)
+        
+        bot_rect = pygame.Rect(bot.x,bot.y,bot.width,bot.height)
+        
+        times = times - 1
+        bot.draw(window)
+        p1.move(keys,ball.y)
+        times,p1_score,p2_score = ball.move(p1_score,p2_score,times,p1.x,p2.x,p1.y,p2.y)
+        
+        bot.move(keys,ball.y)
+        ball.draw(window)
+        p1.draw(window)
+        
+        if re <= 0:
+            if colision1(ball_rect,p1_rect):
+                ball.dx *= -1
+                re = 150
+        
+        if er <= 0:
+            if colision1(ball_rect,bot_rect):
+                er = 150
+                ball.dx *= -1
+        
     if game == 1:
         text_surface11 = myfont.render(f"{int(p1_score)}", True, (255,255,255))
         window.blit(text_surface11, (150,100))
         text_surface111 = myfont.render(f"{int(p2_score)}", True, (255,255,255))
         window.blit(text_surface111, (550,100))
         times = times - 1
-        p2.move(keys)
-        p1.move(keys)
+        p2.move(keys,ball.y)
+        p1.move(keys,ball.y)
         times,p1_score,p2_score = ball.move(p1_score,p2_score,times,p1.x,p2.x,p1.y,p2.y)
         
         ball.draw(window)
@@ -224,11 +282,14 @@ while True:
             game = 0
             main_menu = 1
             was_holding = True
+            p1_score = 0
+            p2_score = 0
     if keys[pygame.K_ESCAPE]:
         if main_menu == 1:
             if was_holding == False:
                 exit()
     else:
         was_holding = False
-        
+    er -= 1
+    re -= 1
     pygame.display.update()
